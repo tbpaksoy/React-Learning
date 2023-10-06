547cbb11f2a9b9056136f069f8843568e650b834
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-10-06 12:40:03 +0300

    Update DrawPolygon.jsx

--- 6-10-2023/DrawPolygon.jsx
+++ 6-10-2023/DrawPolygon.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-function DrawPolygon(points, color) {
-    return <svg width={100} height={100}>
-        <polygon points={points} fill={color} />
-    </svg >
+function DrawPolygon({ points, color }) {
+    let string = "";
+    for (let e in points) {
+        string += points[e].toString();
+        if (e % 2 == 0) string += ",";
+        else string += " ";
+    }
+    string = string.slice(0, -1);
+    return (<svg height="500" width="500">
+        <polygon points={string} style={{ fill: color }} />
+    </svg>)
+        ;
 }
 
 export default DrawPolygon;
