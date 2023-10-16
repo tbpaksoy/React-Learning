82e730b35ab90755bb212c12067c4dcdb49fcc37
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-10-16 11:23:26 +0300

    16.10.2023

---
+++ 11-10-2023/SocialCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SocialCard(firstName, familyName, imabe) {
+    return (<div style={{ background: ' linear-gradient(#EDEBD7 0%, #EDEBD7 50%, #A39594 50%, #A39594 100%)', borderRadius: 25, height: 500, width: 500, position: "relative" }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#E3B23C', width: 100, height: 100, right: 187.5, top: 187.5, position: 'absolute', borderRadius: 50, padding: 25 }}>Test</div>
+    </div>);
+}
+
+export default SocialCard; 
