d5f6939bab6a6ffabbc9ef6b4d7f350309f7d055
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-10-30 14:03:32 +0300

    30.10.2023

--- 27.10.2023/Resume.jsx
+++ 27.10.2023/Resume.jsx
@@ -3,14 +3,17 @@ import './Resume.css'
 function Summary({ name, surname }) {
     return <div className="summary">
         Hello, my name is {name} <b>{surname}</b>, I am from Antalya, Turkey and I am currently working on React.
-        <LinkButton />
+        <div style={{ flexDirection: "row" }}>
+            <LinkButton link={"https://github.com/tbpaksoy"} image={"https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"} />
+            <LinkButton link={"https://tr.linkedin.com/in/tbpaksoy"} image={"https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"} />
+        </div>
     </div>
 }
 function InfoBoxAtDown() {
     return <div style={{ width: "100vw", height: "20vh", alignSelf: "down", bottom: 100 }}></div>
 }
 function LinkButton({ link, image }) {
-    return <a href="https://github.com/tbpaksoy" style={{ position: "relative" }}><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" className="link-button"></img></a>
+    return <a href={link} style={{ position: "relative", height: 150, width: 150 }}><img src={image} className="link-button"></img></a>
 
 }
 function Resume({ name, surname }) {
