e4d605745acff8d2a407c666a38224f18ef97325
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-10-30 13:49:56 +0300

    30.10.2023

--- 27.10.2023/Resume.jsx
+++ 27.10.2023/Resume.jsx
@@ -3,10 +3,15 @@ import './Resume.css'
 function Summary({ name, surname }) {
     return <div className="summary">
         Hello, my name is {name} <b>{surname}</b>, I am from Antalya, Turkey and I am currently working on React.
+        <LinkButton />
     </div>
 }
 function InfoBoxAtDown() {
     return <div style={{ width: "100vw", height: "20vh", alignSelf: "down", bottom: 100 }}></div>
+}
+function LinkButton({ link, image }) {
+    return <a href="https://github.com/tbpaksoy" style={{ position: "relative" }}><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" className="link-button"></img></a>
+
 }
 function Resume({ name, surname }) {
     return <div className="resume">
