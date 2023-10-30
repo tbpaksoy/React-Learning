350ba922fad443be2c2f3b3ee5dfc7d302d13b6a
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-10-30 15:13:10 +0300

    30.10.2023

--- 27.10.2023/Resume.jsx
+++ 27.10.2023/Resume.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import './Resume.css'
 function Summary({ name, surname }) {
     return <div className="summary">
-        Hello, my name is {name} <b>{surname}</b>, I am from Antalya, Turkey and I am currently working on React.
-        <div style={{ flexDirection: "row" }}>
+        Hello, my name is {name} <b>{surname}</b>, I am from Antalya, Turkey and I am currently working on React. I born in 20th April 1998 in Antalya. Rightnow I am software engineering studen in Fırat University
+        <div className="summary-container">
             <LinkButton link={"https://github.com/tbpaksoy"} image={"https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"} />
             <LinkButton link={"https://tr.linkedin.com/in/tbpaksoy"} image={"https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"} />
         </div>
